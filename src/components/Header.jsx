94e8d4945685bd3fb6dc343958c909019858b32a
Meobot <nathanrhodes312@gmail.com>
2023-03-12 14:18:54 -0400

    images added

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Header() {
+	return (
+		<div>
+			<div className="flex">
+				<h1>TODO</h1>
+				<img src="/images/icon-sun.svg" alt="" />
+			</div>
+		</div>
+	)
+}
