06225c8f92763a34fa8c276460257cd7b62b6fcd
Meobot <nathanrhodes312@gmail.com>
2023-10-05 21:49:12 -0400

    pivot to using tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,74 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import Header from "./components/Header";
+import TodoForm from "./components/TodoForm";
+import TodoList from "./components/TodoList";
+
+export type Todo = {
+	id: string;
+	title: string;
+	completed: boolean;
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+	// State to store all todos
+	const [todos, setTodos] = useState<Todo[]>([]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	// Function to generate a UUID
+	function generateUUID() {
+		return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(
+			/[xy]/g,
+			function (c) {
+				const r = (Math.random() * 16) | 0,
+					v = c === "x" ? r : (r & 0x3) | 0x8;
+				return v.toString(16);
+			}
+		);
+	}
+
+	const addTodo = (title: string) => {
+		// Create a new todo object with a unique ID
+		const newTodo = {
+			id: generateUUID(),
+			title: title,
+			completed: false,
+		};
+
+		setTodos([...todos, newTodo]);
+	};
+
+	const deleteTodo = (id: string) => {
+		// Update the state to add the new todo to the array
+		setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+	};
+
+	const toggleTodo = (id: string) => {
+		setTodos((prevTodos) =>
+			prevTodos.map((todo) =>
+				todo.id === id ? { ...todo, completed: !todo.completed } : todo
+			)
+		);
+	};
+
+	const updateTodoTitle = (id: string, newTitle: string) => {
+		setTodos((prevTodos) =>
+			prevTodos.map((todo) =>
+				todo.id === id ? { ...todo, title: newTitle } : todo
+			)
+		);
+	};
+
+	return (
+		<div className="text-white flex flex-col items-center h-screen space-y-6">
+			<Header />
+			<TodoForm addTodo={addTodo} />
+			<TodoList
+				todos={todos}
+				deleteTodo={deleteTodo}
+				toggleTodo={toggleTodo}
+				updateTodoTitle={updateTodoTitle}
+			/>
+		</div>
+	);
 }
 
-export default App
+export default App;
