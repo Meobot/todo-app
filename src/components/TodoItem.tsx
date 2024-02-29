d92a3d60a00b9ac7abeb99d0f5180c845c5ca11e
Meobot <nathanrhodes312@gmail.com>
2024-02-28 18:15:50 -0600

    added drag and drop

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,68 +1,87 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 type PropTypes = {
-  title: string;
-  id: string;
-  completed: boolean;
-  deleteTodo: (id: string) => void;
-  toggleTodo: (id: string) => void;
-  updateTodoTitle: (id: string, newTitle: string) => void; // New prop for updating the title
+	title: string;
+	id: string;
+	completed: boolean;
+	deleteTodo: (id: string) => void;
+	toggleTodo: (id: string) => void;
+	updateTodoTitle: (id: string, newTitle: string) => void;
+	provided: unknown;
 };
 
-function TodoItem({ title, deleteTodo, id, toggleTodo, completed, updateTodoTitle }: PropTypes) {
-  const [isEditing, setIsEditing] = useState(false);
-  const [editedTitle, setEditedTitle] = useState(title); // Initialize editedTitle with the current title
+function TodoItem({
+	title,
+	deleteTodo,
+	id,
+	toggleTodo,
+	completed,
+	updateTodoTitle,
+	provided,
+}: PropTypes) {
+	const [isEditing, setIsEditing] = useState(false);
+	const [editedTitle, setEditedTitle] = useState(title); // Initialize editedTitle with the current title
 
-  const handleTitleClick = () => {
-    setIsEditing(true);
-  };
+	const handleTitleClick = () => {
+		setIsEditing(true);
+	};
 
-  const handleTitleBlur = () => {
-    setIsEditing(false);
-    if (editedTitle !== title) {
-      // Call the callback function to update the title in the parent component
-      updateTodoTitle(id, editedTitle);
-    }
-  };
+	const handleTitleBlur = () => {
+		setIsEditing(false);
+		if (editedTitle !== title) {
+			// Call the callback function to update the title in the parent component
+			updateTodoTitle(id, editedTitle);
+		}
+	};
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setEditedTitle(e.target.value);
-  };
+	const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setEditedTitle(e.target.value);
+	};
 
-  return (
-    <div
-      className={`w-full flex justify-center items-center mb-5 rounded-full border-2 border-sky-900 px-5 py-3 ${
-        completed ? 'bg-green-800 border-0' : ''
-      }`}
-    >
-      {isEditing ? (
-        <input
-		className='text-black'
-          type="text"
-          value={editedTitle}
-          onBlur={handleTitleBlur}
-          onChange={handleInputChange}
-          onKeyDown={(e) => {
-            if (e.key === 'Enter') {
-              handleTitleBlur();
-            }
-          }}
-        />
-      ) : (
-        <h2 className="w-full" onClick={handleTitleClick}>
-          {title}
-        </h2>
-      )}
-      <div className="flex space-x-6">
-        <button className="px-4 py-3 rounded-full " onClick={() => toggleTodo(id)}>
-          Toggle
-        </button>
-        <button className="px-4 py-3 rounded-full " onClick={() => deleteTodo(id)}>
-          Delete
-        </button>
-      </div>
-    </div>
-  );
+	return (
+		<li
+			ref={provided.innerRef}
+			{...provided.draggableProps}
+			{...provided.dragHandleProps}
+			className={`w-full flex justify-center items-center mb-5 rounded-full border-2 border-sky-900 px-5 py-3 ${
+				completed ? "bg-green-800 border-0" : ""
+			}`}
+			id={id}
+		>
+			{isEditing ? (
+				<input
+					className="text-black"
+					type="text"
+					value={editedTitle}
+					onBlur={handleTitleBlur}
+					onChange={handleInputChange}
+					onKeyDown={(e) => {
+						if (e.key === "Enter") {
+							handleTitleBlur();
+						}
+					}}
+				/>
+			) : (
+				<h2 className="w-full" onClick={handleTitleClick}>
+					{title}
+				</h2>
+			)}
+			<div className="flex space-x-6">
+				<button
+					className="px-4 py-3 rounded-full "
+					onClick={() => toggleTodo(id)}
+				>
+					Toggle
+				</button>
+				<button
+					className="px-4 py-3 rounded-full "
+					onClick={() => deleteTodo(id)}
+				>
+					Delete
+				</button>
+			</div>
+		</li>
+	);
 }
 
 export default TodoItem;
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,29 +1,63 @@
-import TodoItem  from "./TodoItem"
-import { Todo } from "../App"
+import TodoItem from "./TodoItem";
+import { Todo } from "../App";
+import { DragDropContext, Droppable, Draggable } from "@hello-pangea/dnd";
 
 type PropTypes = {
 	todos: Todo[];
+	setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
 	deleteTodo: (id: string) => void;
 	toggleTodo: (id: string) => void;
 	updateTodoTitle: (id: string, newTitle: string) => void;
-}
+};
 
-export default function TodoList({ todos, deleteTodo, toggleTodo, updateTodoTitle }: PropTypes) {
+export default function TodoList({
+	todos,
+	setTodos,
+	deleteTodo,
+	toggleTodo,
+	updateTodoTitle,
+}: PropTypes) {
 
-  return (
-    <ul className="w-screen max-w-4xl flex flex-col justify-center items-center">
-      {todos.length === 0 && "No Todos"}
-      {todos.map(todo => {
-        return (
-          <TodoItem
-            {...todo}
-            key={todo.id}
-			deleteTodo={deleteTodo}
-			toggleTodo={toggleTodo}
-			updateTodoTitle={updateTodoTitle}
-          />
-        )
-      })}
-    </ul>
-  )
+	const handleOnDragEnd = (result: { source: { index: number; }; destination: { index: number; }; }) => {
+		const items = Array.from(todos);
+		const [reorderedItem] = items.splice(result.source.index, 1);
+		items.splice(result.destination.index, 0, reorderedItem)
+
+		setTodos(items)
+	}
+
+	return (
+		<DragDropContext onDragEnd={handleOnDragEnd}>
+			<Droppable droppableId="todos-list">
+				{(provided) => (
+					<ul
+						className="todos-list w-screen max-w-4xl flex flex-col justify-center items-center"
+						{...provided.droppableProps}
+						ref={provided.innerRef}
+					>
+						{todos.length === 0 && "No Todos"}
+						{todos.map((todo, index) => {
+							return (
+								<Draggable
+									key={todo.id}
+									draggableId={todo.id}
+									index={index}
+								>
+									{(provided: unknown) => (
+										<TodoItem
+											{...todo}
+											provided={provided}
+											deleteTodo={deleteTodo}
+											toggleTodo={toggleTodo}
+											updateTodoTitle={updateTodoTitle}
+										/>
+									)}
+								</Draggable>
+							);
+						})}
+					</ul>
+				)}
+			</Droppable>
+		</DragDropContext>
+	);
 }
